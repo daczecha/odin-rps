8b0b1b3702767f9273575084a82ef4282df5c0ae
daczecha <daczecha@gmail.com>
2021-11-10 16:08:37 +0400

    Add styles, images. Finish project

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
-function Compare(cS, pS,score){//computerSelection, playerSelection, maxScore
+let roundCounter = 1;
+
+const computerPick = document.getElementById("computer-pick");
+const footer = document.getElementById("footer");
+
+const modal = document.getElementById("modal");
+const gameOverDark = document.getElementById("game-over-dark");
+
+function Compare(cS, pS){//computerSelection, playerSelection, maxScore
+
     if(cS === pS){
-        console.log("Tie");
-        console.log(`${playerScore} | ${computerScore}`);
+        footer.innerText = "TIE!";
     } else if(
               (cS === "rock" &&  pS === "scissors") ||
               (cS === "scissors" &&  pS === "paper") ||
@@ -15,49 +23,68 @@ function Compare(cS, pS,score){//computerSelection, playerSelection, maxScore
              ){
 
         computerScore++;
-        if(computerScore<score){
-
-            console.log("Lose");
-            console.log(`${playerScore} | ${computerScore}`);
-
-        }else{
-            console.log("You lost :(");
-            console.log(`${playerScore} | ${computerScore}`);
-        }
+        document.getElementById(`computer-score-${computerScore}`).classList.remove("far");
+        document.getElementById(`computer-score-${computerScore}`).classList.add("fas");
+        footer.innerText = "YOU LOSE!";
 
     } else{
         playerScore++;
-        if(playerScore <score){
-            console.log("Win");
-            console.log(`${playerScore} | ${computerScore}`);
-        }else{
-            console.log("You win!");
-            console.log(`${playerScore} | ${computerScore}`);
-        }
+        document.getElementById(`player-score-${playerScore}`).classList.remove("far");
+        document.getElementById(`player-score-${playerScore}`).classList.add("fas");
+        footer.innerText = "YOU WIN!";
     }
+    setTimeout(() => {
+        computerPick.src = "img/questionmark.png";
+        computerPick.classList.add("questionmark");
+        footer.innerText = "";
+        document.getElementById("round").innerText = "ROUND " + ++roundCounter;
+    }, 2000);
 }
 
 function ComputerPlay(){
     let rps = ["rock", "paper", "scissors"];
     computerSelection = rps[RandInt(rps.length)];
+    computerPick.classList.remove("questionmark");
+    setTimeout(() => {
+        computerPick.src = `img/${computerSelection}.png`;
+    }, 500);
 }
 
 
 function Game(id){
-    switch(id){
-        case "rock":
-            playerSelection = "rock";
-            break;
-        case "paper":
-            playerSelection = "paper";
-            break;
-        case "scissors":
-            playerSelection = "scissors";
-            break;
-        default:
+    if(playerScore < 3 && computerScore < 3){ 
+        switch(id){
+            case "rock":
+                playerSelection = "rock";
+                break;
+            case "paper":
+                playerSelection = "paper";
+                break;
+            case "scissors":
+                playerSelection = "scissors";
+                break;
+            default:
+        }
+        ComputerPlay();
+            Compare(computerSelection,playerSelection)  
+        
+        if(computerScore>=3){
+            modal.style.display = "flex";
+            gameOverDark.style.display  = "block";
+            document.getElementById("message").innerText = "YOU LOSE!";
+            document.getElementById("message").style.color = "#c23616";
+        }
+        if(playerScore>=3){
+            modal.style.display = "flex";
+            gameOverDark.style.display  = "block";
+            document.getElementById("message").innerText = "YOU WIN!";
+            document.getElementById("message").style.color = "#44bd32";
+        }
     }
-    ComputerPlay();
-    Compare(computerSelection,playerSelection, 1);
+}
+
+function Restart(){
+    location.reload();
 }
 
 function RandInt(range){

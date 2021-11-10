8b9dc53981681da60c67e578cc58926e4d11df87
daczecha <daczecha@gmail.com>
2021-11-10 12:42:30 +0400

    Add player selection

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ function ComputerPlay(){
     return rps[RandInt(rps.length)];
 }
 
+function PlayerPlay(id){
+    switch(id){
+        case "rock":
+            return "rock";
+            break;
+        case "paper":
+            return "paper";
+            break;
+        case "scissors":
+            return "scissors";
+            break;
+        default:
+    }
+}
+
 function RandInt(range){
     return Math.floor(Math.random()*range);
 }
